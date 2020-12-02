ac658c65fa23cb860fb8c411198511e739e2f389
Hipertay <fordarkman@gmail.com>
2020-12-02 11:59:48 +0300

    v.12

---
+++ Assets/Scripts/GameHelper.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class GameHelper : MonoBehaviour
+{
+    public float BoxSize;               // вместимость
+    public float percentWin;            //процент вместимости для победы
+    public float timerCoroutine;        //время ожирания корутины
+    public float timerSpawnTarget;      // пауза при спавне Target
+    public int maxTrgetInScene;         // максимальное кол-во Target
+
+    public GameObject cattleCorral;     // загон
+    public GameObject pasture;          // пастбище
+    public Animator gateAnimator;       // аниматор забора
+    public GameObject panelRestart;     // панель проигрыша
+    public GameObject panelVin;         //панель победы
+    public List<Target> targets;        //Trgets
+
+    private float secondgametime; 
+
+    public bool folow;                  // двигаемся?
+    public bool endGame = false;        // конец игры
+
+    private float squareTargets;        //сумарная площадь Targets
+    private MeshFilter meshCattleCorral;// загон
+    private MeshFilter meshpasture;     // пастбище
+    private GameObjectPool objectPool;  //pool
+
+
+    private void Awake()
+    {
+        squareTargets = 0;
+        folow = false;
+        objectPool = GetComponent<GameObjectPool>();
+        meshCattleCorral = cattleCorral.GetComponent<MeshFilter>();
+        meshpasture = pasture.GetComponent<MeshFilter>();
+    }
+
+    private void Start()
+    {
+        gateAnimator.SetBool("isOpened", false);
+        StartGame();
+        objectPool.caches[0].cacheSize = maxTrgetInScene;
+        objectPool.OnInitialized();
+    }
+
+    private void Update()
+    {
+        if (!endGame)
+        {
+            int i = 0;
+            while (i < Input.touchCount)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Stationary || Input.GetTouch(i).phase == TouchPhase.Moved ||
+                    Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    folow = true;
+                    if (!gateAnimator.GetBool("isOpened"))
+                    {
+                        gateAnimator.SetBool("isOpened", true);
+                    }
+                }
+                else if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                {
+                    folow = false;
+                    if (gateAnimator.GetBool("isOpened"))
+                    {
+                        gateAnimator.SetBool("isOpened", false);
+                    }
+                    StopGame();
+                }
+                ++i;
+            }
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                folow = true;
+                if (!gateAnimator.GetBool("isOpened"))
+                {
+                    gateAnimator.SetBool("isOpened", true);
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                folow = false;
+                if (gateAnimator.GetBool("isOpened"))
+                {
+                    gateAnimator.SetBool("isOpened", false);
+                }
+                StopGame();
+            }
+#endif
+
+            if (folow)
+            {
+                secondgametime += Time.deltaTime;
+                if (secondgametime >= timerSpawnTarget)
+                {
+                    GameObject ob =  GameObjectPool.Spawn(objectPool.caches[0].prefab, GetRandomPos(pasture, meshpasture), Quaternion.Euler(0f, Random.Range(0, 360f), 0f));
+                    NavMeshAgent navMeshAgent = ob.GetComponent<NavMeshAgent>();
+                    navMeshAgent.SetDestination(GetRandomPos(cattleCorral, cattleCorral.GetComponent<MeshFilter>()));
+                    Animator animator = ob.GetComponent<Animator>();
+                    animator.SetInteger("animation", 1);
+                    animator.speed = navMeshAgent.speed + ob.GetComponent<Target>().SpedAnimationTarget;
+                    secondgametime = 0;
+                }
+            }
+
+        }
+
+    }
+
+    private void StartGame()
+    {
+        for (int i = 0; i < objectPool.caches.Length; i++)
+        {
+            for (int o = 0; o < objectPool.caches[i].cacheSize; o++)
+            {
+                GameObjectPool.Spawn(objectPool.caches[i].prefab, GetRandomPos(pasture, meshpasture), Quaternion.Euler(0f, Random.Range(0, 360f), 0f));
+
+            }
+
+        }
+    }
+
+    private void StopGame()
+    {
+        
+        foreach (Target target in targets)
+        {
+            squareTargets += target.square;
+        }
+
+        float temp = BoxSize * percentWin;
+        float percent = temp;
+
+        if (squareTargets >= percent && squareTargets <= BoxSize)
+        {
+            StartCoroutine(WaitDrawUI(panelVin));
+        }
+        else
+        {
+            StartCoroutine(WaitDrawUI(panelRestart));
+        }
+
+        while (squareTargets > BoxSize)
+        {
+            int t = Random.Range(0, targets.Count);
+            squareTargets -= targets[t].square;
+            targets[t].KickOut();
+            targets.RemoveAt(t);
+        }
+        endGame = true;
+    }
+
+    IEnumerator WaitDrawUI(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(timerCoroutine);
+        gameObject.SetActive(true);
+    }
+
+    public Vector3 GetRandomPos(GameObject @object, MeshFilter mesh)
+    {
+        List<Vector3> VerticeList = new List<Vector3>(mesh.sharedMesh.vertices);
+        Vector3 leftTop = @object.transform.TransformPoint(VerticeList[0]);
+        Vector3 rightTop = @object.transform.TransformPoint(VerticeList[10]);
+        Vector3 leftBottom = @object.transform.TransformPoint(VerticeList[110]);
+        Vector3 rightBottom = @object.transform.TransformPoint(VerticeList[120]);
+        Vector3 XAxis = rightTop - leftTop;
+        Vector3 ZAxis = leftBottom - leftTop;
+        Vector3 RndPointonPlane = leftTop + XAxis * Random.value + ZAxis * Random.value;
+
+        return RndPointonPlane;
+    }
+
+    public void Restart()
+    {
+        panelRestart.SetActive(false);
+        panelVin.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour
+{
+    public bool flyobject;
+    public float square;              //площадь
+    public int scaledPower;           //кратность увеличения
+    public float minSpedTarget;       //скорость персонажа min
+    public float maxSpedTarget;       //скорость персонажа max
+    public float SpedAnimationTarget; //скорость анимации персонажа
+    public float spedRotation;        //скорость поворота
+    public float minPositionFly;       // min прыжок
+    public float maxPositionFly;       // max прыжок
+    public bool gravitation;           //гравитация on/off
+    public float updatePosition;       //расстояние для обновления позиции
+
+    GameHelper gameHelper;
+    bool onGate;
+    bool scaled;
+    bool touch;
+    MeshFilter meshFilter;
+    Animator animator;
+    Rigidbody rigidbody;
+    Vector3 randomPosition;
+
+
+    public NavMeshAgent Agent { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        touch = false;
+        scaled = false;
+        flyobject = false;
+        gravitation = true;
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.speed = Random.Range(minSpedTarget, maxSpedTarget);
+        animator = GetComponent<Animator>();
+        gameHelper = FindObjectOfType<GameHelper>();
+        meshFilter = gameHelper.cattleCorral.GetComponent<MeshFilter>();
+        rigidbody = GetComponent<Rigidbody>();
+        onGate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var look = Quaternion.LookRotation(randomPosition - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, look, spedRotation * Time.deltaTime);
+        var heading = randomPosition - transform.position;
+        if (heading.sqrMagnitude <= updatePosition)
+        {
+            randomPosition = gameHelper.GetRandomPos(gameHelper.cattleCorral, meshFilter);
+            if (Agent.enabled)
+            {
+                Agent.SetDestination(randomPosition);
+                Agent.isStopped = false;
+                Agent.speed = minSpedTarget;
+            }
+        }
+        if (!touch)
+        {
+            int i = 0;
+            while (i < Input.touchCount)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Stationary || Input.GetTouch(i).phase == TouchPhase.Moved ||
+                    Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    Folov();
+                }
+                else if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                {
+                    Stay();
+                    if (onGate)
+                        Scaled(scaledPower);
+                    touch = true;
+                }
+                ++i;
+            }
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                Folov();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                Stay();
+                if (onGate)
+                    Scaled(scaledPower);
+                touch = true;
+            }
+#endif
+        }
+    }
+
+    public void Folov()
+    {
+       randomPosition = gameHelper.GetRandomPos(gameHelper.cattleCorral, meshFilter);
+        if (Agent.enabled)
+        {
+            Agent.SetDestination(randomPosition);
+            
+            Agent.isStopped = false;
+        }
+
+        animator.SetInteger("animation", 1);
+        animator.speed = Agent.speed + SpedAnimationTarget;
+
+    }
+
+    public void Stay()
+    {
+        animator.SetInteger("animation", 0);
+        if (Agent.enabled)
+            Agent.isStopped = true;
+    }
+
+    public void Scaled(int i)
+    {
+        if (!scaled)
+        {
+            this.gameObject.transform.localScale = gameObject.transform.localScale * i;
+            scaled = true;
+        }
+    }
+        
+
+    public void KickOut()
+    {
+        if(!gravitation) rigidbody.useGravity = false;
+        Agent.enabled = false;
+        flyobject = true;
+        rigidbody.AddRelativeForce(new Vector3(Random.Range(minPositionFly, maxPositionFly), maxPositionFly, Random.Range(minPositionFly, maxPositionFly)), ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gate"))
+        {
+            gameHelper.targets.Add(this.gameObject.GetComponent<Target>());
+            onGate = true;
+            Folov();
+        }
+    }
+
+   
+
+
+
+
+
+
+}
